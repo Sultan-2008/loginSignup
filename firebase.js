1870a8498f138ea7e6afb848d6680be1ab5f0563
Sultan-2008 <152035207+Sultan-2008@users.noreply.github.com>
2024-03-02 09:43:23 -0700

    add

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBANQCHN_aSw-QBMWkM9Q3NxgEURHTM0Yg",
+    authDomain: "testapp-42fcc.firebaseapp.com",
+    projectId: "testapp-42fcc",
+    storageBucket: "testapp-42fcc.appspot.com",
+    messagingSenderId: "914467538654",
+    appId: "1:914467538654:web:b252857baf83b09dfea44f"
+};
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
